57966f8c1485e326176c236e016dc50210c65c31
Jason Sio <son8292003@gmail.com>
2020-09-16 04:16:46 +0000

    update url and host config

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'https://code-4-food.life/resume/cms'),
   admin: {
+    host: env('ADMIN_HOST', 'https://code-4-food.life/resume/cms'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4466e4ef92e5471b3a9c07db2a67b7e2'),
     },
